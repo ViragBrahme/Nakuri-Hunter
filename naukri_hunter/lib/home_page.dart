afa9fc56607a28012108d14b17349f1805a0e62a
Virag Brahme <virag.brahme@globant.com>
2019-06-01 14:03:01 +0530

    Adding multiple job search options

--- naukri_hunter/lib/home_page.dart
+++ naukri_hunter/lib/home_page.dart
@@ -8,29 +8,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController myNameTextEditCont = new TextEditingController();
+  TextEditingController skillsTextEditCont = new TextEditingController();
+  TextEditingController technologyTextEditCont = new TextEditingController();
+  TextEditingController noticePeriodTextEditCont = new TextEditingController();
+  TextEditingController prefferedLocTextEditCont = new TextEditingController();
+  TextEditingController expectedCTCTextEditCont = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Naukri Hunter"),),
+      appBar: AppBar(
+        title: Text("Naukri Hunter"),
+      ),
       body: SafeArea(
         child: ListView(
           children: <Widget>[
             TextField(
-              controller: myNameTextEditCont,
+              controller: skillsTextEditCont,
               decoration: InputDecoration(
                   labelText: 'Skills', contentPadding: EdgeInsets.all(20.0)),
             ),
             TextField(
+              controller: technologyTextEditCont,
               decoration: InputDecoration(
                   labelText: 'Technologies',
+                  contentPadding: EdgeInsets.all(20.0)),
+            ),
+            TextField(
+              controller: noticePeriodTextEditCont,
+              decoration: InputDecoration(
+                  labelText: 'Notice Period',
+                  contentPadding: EdgeInsets.all(20.0)),
+            ),
+            TextField(
+              controller: prefferedLocTextEditCont,
+              decoration: InputDecoration(
+                  labelText: 'Preffered Location',
+                  contentPadding: EdgeInsets.all(20.0)),
+            ),
+            TextField(
+              controller: expectedCTCTextEditCont,
+              decoration: InputDecoration(
+                  labelText: 'Expected CTC',
                   contentPadding: EdgeInsets.all(20.0)),
             ),
             Padding(padding: const EdgeInsets.all(10.0)),
             RaisedButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SearchResult()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchResult()));
               },
               child: Text('Click Me'),
             )
